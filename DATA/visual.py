6f4a44bc76a0bfb8e619cc70075eb00e504fceab
rciomoreno <morenoalarconrocio@gmail.com>
2022-12-13 21:01:08 +0100

    [16]

--- DATA/visual.py
+++ DATA/visual.py
@@ -7,28 +7,31 @@ st.title('ELIGE TUS COLORES')
 st.header('Proyecto Final: Aprende a maquillarte en función de tu colometría')
 
 combinaciones = pd.read_csv('../DATA/TABLA_BASE/combinaciones_final_csv.csv')
+recomendaciones = pd.read_csv('../DATA/TABLA_RECOMENDACIONES/prod_recomendados_csv.csv')
+
 
 cabello, croma_cab = st.columns(2)
 ojos, croma_ojo = st.columns(2)
 piel,croma_piel= st.columns(2)
 
 with cabello:
-    cabello = st.selectbox('Elige tu color de cabello', combinaciones.CABELLO.unique())
+    cabello = st.sidebar.selectbox('Elige tu color de cabello', combinaciones.CABELLO.unique())
 
 with croma_cab:
-    croma_cab = st.radio('1 color de pelo o más de 1 color', combinaciones.CROMA_CAB.unique())
+    croma_cab = st.sidebar.radio('1 color de pelo o más de 1 color', combinaciones.CROMA_CAB.unique())
 
 with ojos:
-    ojos = st.selectbox('Elige tu color de ojos', combinaciones.OJOS.unique())
+    ojos = st.sidebar.selectbox('Elige tu color de ojos', combinaciones.OJOS.unique())
 
 with croma_ojo:
-    croma_ojo = st.radio('1 solo color de ojos o más de 1 color', combinaciones.CROMA_OJOS.unique())
+    croma_ojo = st.sidebar.radio('1 solo color de ojos o más de 1 color', combinaciones.CROMA_OJOS.unique())
 
 with piel:
-    piel = st.selectbox('Elige tu color de piel', combinaciones.PIEL.unique())
+    piel = st.sidebar.selectbox('Elige tu color de piel', combinaciones.PIEL.unique())
 
 with croma_piel:
-    croma_piel = st.radio('1 solo color de piel o más de 1 color', combinaciones.CROMA_PIEL.unique())
+    croma_piel = st.sidebar.radio('1 solo color de piel o más de 1 color', combinaciones.CROMA_PIEL.unique())
+
 
 
 resultado = combinaciones[(combinaciones.CABELLO == cabello) &
@@ -45,10 +48,12 @@ hide_dataframe_row_index = """
             .row_heading.level0 {display:none}
             .blank {display:none}
             </style>
-            """
-            
+            """      
+
 st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
 
-res = st.table(resultado.PALETAS)
-
-st.table(combinaciones.CABELLO)
+if st.button('Calcular paleta'):
+    var = resultado.PALETAS[0]
+    st.header(var)
+    st.dataframe(recomendaciones[recomendaciones['PALETA'].str.contains(var)][['NOMBRE', 'PRODUCTO', 'TIENDA', 'LINK', 'PRECIO (€)']])
+    #nuevo = st.slider('hola', 0,200,0)
